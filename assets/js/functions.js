53371b1c113e1250276746f42f92731f8681e5bb
Rasoul Baghban <rasoul.baghban@hotmail.com>
2014-11-13 00:19:09 +0330

    add jquery to animate subnav  header

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function() 
+{
 
+var sub_nav_header_links = $('#sub_header_links > a');
+var $root = $('html, body');
+
+$(sub_nav_header_links).click(function(){
+   $root.animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 72
+    }, 500);
+    return false;
 });
 
+});
